5ad3ea15bc7d37b16bb330200e92839e1783051b
Omar Rodriguez Soto <omar.rodriguez@mapfre.com>
2018-03-16 18:56:55 +0100

    Añadidos analizadores shodan, otx y whois!

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,29 +1,37 @@
 import configparser
-
+import os
 import logging
 FORMAT = "%(asctime)-15s %(clientip)s %(user)-8s %(message)s"
 logging.basicConfig(format=FORMAT)
 
-def analyze_ip(ip):
-    analyzer = IP(ip)
+def analyze_ip(ip, analyzer):
+    analyzer = IPAnalyzer(ip, analyzer)
     analyzer.analyze()
     return analyzer.report()
-
 
 def analyze_domain(domain):
     return "TU DOMINIO ES %s" % domain
 
 class Analyzer:
+    analyzers = [
+        'all',
+        "shodan",
+        "whois",
+        "otx"
+    ]
+    
     def __init__(self, *args):
-        if args is not None or not len(args):
+        if args is None or not len(args) or args[0] == 'all':
             self.all = True
         else:
             self.all = False
-            self.options_enabled = args
+            self.options_enabled = [ elem for elem in args ]
 
         # Cogemos las variables de configuracion
         settings = configparser.ConfigParser()
-        settings.read('tokens.conf')
+        # rutas absolutas desde el propio fichero
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        settings.read(os.path.join(dirname, "..", 'tokens.conf'))
         self.TOKENS = settings._sections['Tokens']
 
 
@@ -44,13 +52,13 @@ class Domain(Analyzer):
         pass
 
 
-class IP(Analyzer):
+class IPAnalyzer(Analyzer):
+
     def __init__(self, ip, *args):
         self.ip = ip
-        super(IP, self).__init__(args)
-        self.analyzers = [
-            "shodan"
-        ]
+        super(IPAnalyzer, self).__init__(*args)
+        self.results = {}
+        
 
 
     def analyze(self):
@@ -62,15 +70,27 @@ class IP(Analyzer):
         for analyzer in self.analyze_opts:
             if analyzer == "shodan":
                 self.analyze_shodan()
+            elif analyzer == "whois":
+                self.analyze_whois()
+            elif analyzer == "otx":
+                self.analyze_otx()
 
 
     def report(self):
-        return self.host
+        return self.results
 
+    def analyze_whois(self):
+        from ipwhois import IPWhois
+        whois = IPWhois(self.ip)
+        self.results['whois'] = whois.lookup_rdap()
 
+    def analyze_otx(self):
+        from OTXv2 import OTXv2, IndicatorTypes
+        self.otx=OTXv2(self.TOKENS["otx"])
+        otx_results = self.otx.get_indicator_details_full(IndicatorTypes.IPv4, self.ip)
+        self.results["otx"] = otx_results 
 
     def analyze_shodan(self):
-
         from shodan import Shodan
         try:
             shodan_api = Shodan(self.TOKENS["shodan"])
@@ -79,5 +99,5 @@ class IP(Analyzer):
 
         # Lookup the host
         host = shodan_api.host(self.ip)
-        self.host = host
+        self.results['shodan'] = host
 
--- src/webservice.py
+++ src/webservice.py
@@ -2,21 +2,28 @@ from flask import Flask
 from flask import request, abort, jsonify
 import json
 
-from .analyzer import analyze_ip, analyze_domain
+from .analyzer import analyze_ip, analyze_domain, Analyzer
 
 ws = Flask("ipanalyzer")
 
 @ws.route("/analyze")
-def ip_analyze():
+@ws.route("/analyze/")
+@ws.route("/analyze/<analyzer>")
+@ws.route("/analyze/<analyzer>/")
+def ip_analyze(analyzer = None):
     ip = request.args.get('ip')
     domain = request.args.get('domain')
+    if analyzer is None:
+        analyzer = "all"
+    elif not analyzer in Analyzer.analyzers:
+        return abort(400, "Analizador invalido")
 
     if ip:
-        return jsonify(analyze_ip(ip))
+        return jsonify(analyze_ip(ip, analyzer))
     elif domain:
         return analyze_domain(domain)
     else:
-        return abort(418)
+        return abort(418, "I don't like coffee")
 
 
 if __name__ == "__main__":
