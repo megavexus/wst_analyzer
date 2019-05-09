aa1dfd610e2affd489ee5c345dac6e4b02b87be9
megavexus <nexus.megavexus@gmail.com>
2019-05-09 16:37:05 +0200

    Añadido soporte a proxy y nueva clase refractorizada

--- src/wst_analyzer/analyzer/ipAnalyzer.py
+++ src/wst_analyzer/analyzer/ipAnalyzer.py
@@ -3,9 +3,9 @@ from .scanTools import ScanTools
 
 class IPAnalyzer(Analyzer):
 
-    def __init__(self, ip, *args):
+    def __init__(self, ip, *args, **kwargs):
         self.ip = ip
-        super(IPAnalyzer, self).__init__(*args)
+        super(IPAnalyzer, self).__init__(*args, **kwargs)
 
     def analyze(self):
         if not self.all:
@@ -14,15 +14,14 @@ class IPAnalyzer(Analyzer):
         else:
             self.analyze_opts = self.analyzers
 
+        scan_tools = ScanTools(tokens=self.TOKENS, proxy=self.proxy)
         for analyzer in self.analyze_opts:
             if analyzer == "shodan":
-                self.results['shodan'] = ScanTools.shodan(
-                    self.TOKENS["shodan"], ip=self.ip)
+                self.results['shodan'] = scan_tools.shodan(ip=self.ip)
             elif analyzer == "whois":
-                self.results['whois'] = ScanTools.whois(ip=self.ip)
+                self.results['whois'] = scan_tools.whois(ip=self.ip)
             elif analyzer == "otx":
-                self.results['otx'] = ScanTools.otx(
-                    self.TOKENS["otx"], ip=self.ip)
+                self.results['otx'] = scan_tools.otx(ip=self.ip)
 
     def report(self):
         return self.results
