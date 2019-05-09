10ab049ef3ad2648c92b301fe04d77ce0a8a7b34
megavexus <nexus.megavexus@gmail.com>
2019-05-09 16:32:45 +0200

    feat: refractorizado scantools para no se una clase estática

--- src/wst_analyzer/analyzer/scanTools.py
+++ src/wst_analyzer/analyzer/scanTools.py
@@ -1,60 +1,98 @@
 from .exceptions import *
-from retrying import retry
+from urllib.request import ProxyHandler
 
-NUM_MAX_RETRY=7
+import socket
+import whois
+from ipwhois import IPWhois
+
+from shodan import Shodan, exception
+from OTXv2 import OTXv2, IndicatorTypes
+
+from retrying import retry
+NUM_MAX_RETRY=3
 
 class ScanTools(object):
-    @staticmethod
-    @retry(stop_max_attempt_number=NUM_MAX_RETRY) 
-    def whois(ip=None, domain=None):
+
+    def __init__(self, tokens, proxy=None):
+        self.tokens = tokens
+        self.proxy = proxy
+        if self.proxy:
+            self.proxy_dict = {
+                'http': proxy,
+                'https': proxy,
+                'ftp': proxy
+            }
+
+        self.setup_otx()
+        self.setup_shodan()
+        self.setup_whois()
+        
+    def setup_whois(self):
+        self.proxy_handler = None
+        if self.proxy:
+            self.proxy_handler = ProxyHandler(self.proxy_dict)
+
+    def setup_otx(self):
+        self.otx_api = None
+        if self.tokens.get('otx'):
+            self.otx_api = OTXv2(self.tokens.get('otx'), proxy=self.proxy)
+
+    def setup_shodan(self):
+        self.shodan_api = None
+        if self.tokens.get('shodan'):
+            self.shodan_api = Shodan(self.tokens.get('shodan'), proxies=self.proxy_dict)
+
+    #@retry(stop_max_attempt_number=NUM_MAX_RETRY) 
+    def whois(self, ip=None, domain=None):
         if ip==None and domain==None: 
             raise Exception("No arguments provided")
-        
+
         results = {}
         if ip != None:
-            from ipwhois import IPWhois
             try:
-                ipwhois = IPWhois(ip)
+                ipwhois = IPWhois(
+                    ip, 
+                    proxy_opener=self.proxy_handler,
+                    allow_permutations=True
+                )
             except (ValueError):
                 raise IPNotFoundException(ip)
             results["ip"] = ipwhois.lookup_rdap()   
         if domain != None:
-            import whois
-            results["domain"] = whois.whois(domain)
+            try:
+                results["domain"] = whois.whois(domain)
+            except socket.gaierror:
+                pass
         return results
 
-    @staticmethod
-    @retry(stop_max_attempt_number=NUM_MAX_RETRY) 
-    def otx(token, ip=None, domain=None):
-        from OTXv2 import OTXv2, IndicatorTypes
-        otx=OTXv2(token)
+    
+    #@retry(stop_max_attempt_number=NUM_MAX_RETRY) 
+    def otx(self, ip=None, domain=None):
         if ip==None and domain==None: 
             raise Exception("No arguments provided")
-        
+        if self.otx_api == None:
+            return {"status": "err", "reason": "No api token"}
+
         results = {}
         if ip != None:
-            results["ip"] = otx.get_indicator_details_full(IndicatorTypes.IPv4, ip)
+            results["ip"] = self.otx_api.get_indicator_details_full(IndicatorTypes.IPv4, ip)
         if domain != None:
-            results["domain"] = otx.get_indicator_details_full(IndicatorTypes.DOMAIN, domain)
+            results["domain"] = self.otx_api.get_indicator_details_full(IndicatorTypes.DOMAIN, domain)
         
         return results 
 
-    @staticmethod
+    
     @retry(stop_max_attempt_number=NUM_MAX_RETRY) 
-    def shodan(token, ip=None, domain=None):
+    def shodan(self, ip=None, domain=None):
         if ip==None and domain==None: 
             raise Exception("No arguments provided")
-        from shodan import Shodan, exception
+        if ip==None: 
+            return {"status": "err", "reason": "No IP provided"}
+        if self.shodan_api == None:
+            return {"status": "err", "reason": "No api token"}
+        
         try:
-            shodan_api = Shodan(token)
-        except Exception as e:
-            print("Ups! Ha ocurrido un error: %s" % e)
-        # Lookup the host
-        if domain != None:
-            import socket
-            ip = socket.gethostbyname(domain)
-        try:
-            host = shodan_api.host(ip)
+            host = self.shodan_api.host(ip)
             return host
         except exception.APIError as e:
             if str(e) == "No information available for that IP.":
