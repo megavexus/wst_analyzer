86d851add5b920a79819b0bc2706c5714447e6d0
jgutierrez <gnjavie@mapfre.com>
2021-02-18 18:16:56 +0100

    Actualizado setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
 from glob import glob
 
-REQUIREMENTS = [str(ir.req) for ir in parse_requirements(
-    'requirements.txt',  session=False)]
+
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
 
 setup(
     name='wst_analyzer',
